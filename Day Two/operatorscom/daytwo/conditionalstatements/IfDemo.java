3626967e0d287ce886dba9f46b1fee8df41a7ea6
pranalichaskar7 <pranalichaskar74@gmail.com>
2025-07-30 21:48:12 +0530

    TNS Dailycodes

---
+++ Day Two/operatorscom/daytwo/conditionalstatements/IfDemo.java
@@ -0,0 +1,22 @@
+package com.tnsif.daytwo.operatorscom.daytwo.conditionalstatements;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+int x=7;
+		
+		if(x==5)
+		{
+			System.out.println("Value of x is 5");
+		}
+		
+		int age = 17;
+		
+		if(age > 18)
+		{
+			System.out.println("You are eligible for voting");
+		}
+
+	}
+
+}
