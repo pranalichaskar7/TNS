a4591330121104159ff531353bfc7927fa9dfb5d
pranalichaskar7 <pranalichaskar74@gmail.com>
2025-08-11 09:34:09 +0530

    Your Commit

---
+++ usingfinal/FinalMethodClass.java
@@ -0,0 +1,18 @@
+package com.tnsif.daysix.usingfinal;
+
+public class FinalMethodClass {
+
+	public static void main(String[] args) {
+		final int a = 50;
+		
+		final void show()
+		{
+			System.out.println("Value of a: "+a);
+		}
+
+//		final void show()
+//		{
+//			System.out.println("show Method");
+	//}
+
+}
