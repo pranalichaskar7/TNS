cf16e8ad5cb875d4c2d248ef3bc05ad7b1a68cc1
pranalichaskar7 <pranalichaskar74@gmail.com>
2025-09-08 22:48:39 +0530

    TNS

---
+++ DayTwo/src/com/tnsif/daytwo/operatorscom/daytwo/conditionalstatementscom/daytwo/loopingstatements/ForLoopExample.java
@@ -0,0 +1,13 @@
+package com.tnsif.daytwo.operatorscom.daytwo.conditionalstatementscom.daytwo.loopingstatements;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=100; i++)
+		{
+			System.out.println("Value of i : "+ i);  
+		}
+
+	}
+
+}
---
+++ DayTwo/src/com/tnsif/daytwo/operatorscom/daytwo/conditionalstatementscom/daytwo/loopingstatements/WhileDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.daytwo.operatorscom.daytwo.conditionalstatementscom.daytwo.loopingstatements;
+
+public class WhileDemo {
+
+	public static void main(String[] args) {
+		int i = 1;
+		while(i<=100)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+
+	}
+
+}
